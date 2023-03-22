ee9ffd8ce5cc764fcb08e8a8b7b3a5261308ff9a
JadenLund <lundjaden@gmail.com>
2023-03-22 13:56:50 -0500

    adding in css

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,1 +1,11 @@
-//hi
+import React from 'react';
+
+export default function Square(props) {
+    return (
+        <button onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+
+Square;
